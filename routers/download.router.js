f1e408d569a29ca10e92c4ad71e696963de8453a
Andrew <Andry.sheremetew@gmail.com>
2026-01-10 17:02:43 +0200

    add fix

--- routers/download.router.js
+++ routers/download.router.js
@@ -6,9 +6,10 @@ const router = Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-router.get("/", async (req, res) => {
+router.get("/:fileName", async (req, res) => {
   try {
-    const filePath = join(__dirname, "..", "data", "goods.json");
+    const { fileName } = req.params
+    const filePath = join(__dirname, "..", "data", fileName);
     res.download(filePath, "catalog.json");
   } catch (error) {
     console.log(error);
